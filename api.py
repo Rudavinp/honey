691ea434c19b84becf5de1e19169498958487874
R <rudavinp@gmail.com>
2020-02-04 16:45:02 +0300

    small commit add server func

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
 from webob import Request, Response
 from parse import parse
+
 import gunicorn
 
 class API:
     def __init__(self):
         self.routers = {}
+        print(2, self.routers)
 
     def __call__(self, environ, start_response):
-        print(100, start_response)
+        print(1, self.routers)
         request = Request(environ)
         response = self.handle_request(request, environ)
 
@@ -21,17 +23,14 @@ class API:
         return response(environ, start_response)
 
     def route(self, path):
+        print(4, self.routers)
         def wrapper(handler):
             self.routers[path] = handler
+            print(6, self.routers)
             return handler
         return wrapper
 
     def handle_request(self, request, environ):
-        method = environ['REQUEST_METHOD']
-        path = environ['PATH_INFO']
-        print(22, method)
-        print(22, path)
-        user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found")
         response = Response()
         handler, kwargs =  self.find_handler(request.path)
 
@@ -44,8 +43,8 @@ class API:
 
 
     def find_handler(self, request_path):
+        print(3, self.routers)
         for path, handler in self.routers.items():
-            print(33, path, request_path)
             parse_result = parse(path, request_path)
             if parse_result is not None:
                 return handler, parse_result.named
@@ -53,4 +52,12 @@ class API:
 
     def default_response(self, response):
         response.status_code = 404
-        response.text = 'Not faund'
+        response.text = 'Not found'
+
+def server(host='127.0.0.1', port=8000, app=None):
+    from wsgiref import simple_server
+    server = simple_server.make_server(host, port, app)
+    print('Start Honey server on port {} ....'.format(port))
+    server.serve_forever()
+
+
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-from api import API
+from api import API, server
 
 app = API()
+
+
+
+@app.route('/second/{some}')
+def second(request, response, some):
+    print(5)
+    response.text = 'Hello, it is the second page {}'.format(some)
 
 @app.route('/home/{name}')
 def home(request, response, name):
     response.text= 'Привет,{} это главная страница'.format(name)
 
-@app.route('/second/{some}')
-def second(request, response, some):
-    response.text = 'Hello, it is the second page {}'.format(some)
+
+server(app=app)
